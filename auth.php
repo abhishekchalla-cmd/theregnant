64ae6157c762ef5d2912ff631d96550d5b1d4511
unknown <abhishek-cmd@github.com>
2019-10-03 22:52:13 +0530

    fourth commit

--- auth.php
+++ auth.php
@@ -2,5 +2,5 @@
     $con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
     echo mysqli_fetch_row(mysqli_query($con,"select * from test"))[0];
 
-    echo "<br><h1>What a beautiful day!</h1>";
+    echo "<br><h1>I wish this is the last test commmit!</h1>";
 ?>
