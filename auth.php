3704bb241dd17fe7f9fea98ec2c36e1b64d845cf
unknown <abhishek-cmd@github.com>
2019-10-03 22:43:38 +0530

    third commit

--- auth.php
+++ auth.php
@@ -1,4 +1,6 @@
 <?
     $con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
     echo mysqli_fetch_row(mysqli_query($con,"select * from test"))[0];
+
+    echo "<br><h1>What a beautiful day!</h1>";
 ?>
