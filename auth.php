b5f5ffc6137519fd01c4fa41a8a970f825932eda
unknown <abhishek-cmd@github.com>
2019-11-02 21:49:43 +0530

    Revision 5

--- auth.php
+++
@@ -1,6 +0,0 @@
-<?
-    $con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
-    echo mysqli_fetch_row(mysqli_query($con,"select * from test"))[0];
-
-    echo "<br><h1>The last commit!</h1>";
-?>
