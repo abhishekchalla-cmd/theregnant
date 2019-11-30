35a629803191c3fac670912468cbc88ee2030c42
unknown <abhishek-cmd@github.com>
2019-11-30 15:31:43 +0530

    Mobile Request 1

--- authenticate.php
+++ authenticate.php
@@ -5,6 +5,7 @@
 	$userData="";
 	$con="";
 	$web=false;
+	$mobileReq=false;
 
 	function createNewToken($id){
 		$num=rand($id*10000000000000000,($id+1)*10000000000000000);
@@ -41,6 +42,24 @@
 				if($userData!=""){$success=true;}
 			}
 			catch(Exception $e){}
+		}
+	}
+	elseif(isset($_GET['token']) && isset($_GET['request'])){
+		$token=$_GET['token'];
+		$request=$_GET['request'];
+		if(is_numeric($token)){
+			$con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
+			$que=mysqli_query($con,"select * from tokens where token='" . $token . "'");
+			$uid=mysqli_fetch_assoc($que)['uid'];
+			if(mysqli_num_rows($que)>0){
+				$success=true;
+				$mobileReq=true;
+				if($request=="basicinfo"){
+					$uq=mysqli_fetch_assoc(mysqli_query($con,"select * from users where id=" . $uid . " limit 1"));
+					$str=$token . "<>" . $uq['name'] . "<>" . $uq['profilePic'] . "<>" . $uq['email'];
+					echo $str;
+				}
+			}
 		}
 	}
 	else{
@@ -86,57 +105,59 @@
 		echo "Error occured";
 	}
 	else{
-		$con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
+		if(!$mobileReq){
+			$con=mysqli_connect("localhost","theremyt_usr","regnantREG","theremyt_db");
 
-		$email=$userData['email'];
-		$name=$userData['name'];
-		$profilePic=$userData['picture'];
-		$id=0;
+			$email=$userData['email'];
+			$name=$userData['name'];
+			$profilePic=$userData['picture'];
+			$id=0;
 
-		$draw_req=mysqli_query($con,"select name,email,profilePic,id from users where email='" . $email . "' limit 1");
-		if(mysqli_num_rows($draw_req)>0){
-			$draw_req=mysqli_fetch_row($draw_req);
-			$name=$draw_req[0];
-			$email=$draw_req[1];
-			$profilePic=$draw_req[2];
-			$id=$draw_req[3];
-		}
-		else{
-			mysqli_query($con,"insert into users (name,email,profilePic) values ('" . $name . "','" . $email . "','" . $profilePic . "')");
-			$id=mysqli_fetch_row(mysqli_query($con,"select max(id) from users"))[0];
-		}
-
-		$userData=array(
-			'name' => $name,
-			'email' => $email,
-			'profilePic' => $profilePic,
-			'id' => $id
-		);
-
-		if($web){
-			$_SESSION['name']=$userData['name'];
-			$_SESSION['id']=$userData['id'];
-			$_SESSION['email']=$userData['email'];
-			$_SESSION['profilePic']=$userData['profilePic'];
-			header("Location: index.php");
-		}
-		else{
-			$tokenQ=mysqli_query($con,"select * from tokens where uid=" . $userData['id']);
-			
-			if(mysqli_num_rows($tokenQ)>=5){
-
-				$time=time();$id=0;
-				for($i=0;$i<mysqli_num_rows($tokenQ);$i++){
-					$row=mysqli_fetch_assoc($tokenQ);
-					if($row['time']<$time){$time=$row['time'];$id=$row['id'];}
-				}
-				mysqli_query($con,"delete from tokens where id=" . $id);
-
+			$draw_req=mysqli_query($con,"select name,email,profilePic,id from users where email='" . $email . "' limit 1");
+			if(mysqli_num_rows($draw_req)>0){
+				$draw_req=mysqli_fetch_row($draw_req);
+				$name=$draw_req[0];
+				$email=$draw_req[1];
+				$profilePic=$draw_req[2];
+				$id=$draw_req[3];
+			}
+			else{
+				mysqli_query($con,"insert into users (name,email,profilePic) values ('" . $name . "','" . $email . "','" . $profilePic . "')");
+				$id=mysqli_fetch_row(mysqli_query($con,"select max(id) from users"))[0];
 			}
 
-			$randToken=createNewToken($userData['id']);
-			mysqli_query($con,"insert into tokens (uid,token,time) values (" . $userData['id'] . ",'" . $randToken . "','" . time() . "')");
-			echo $randToken . "<>" . $userData['name'] . "<>" . $userData['profilePic'] . "<>" . $userData['email'];
+			$userData=array(
+				'name' => $name,
+				'email' => $email,
+				'profilePic' => $profilePic,
+				'id' => $id
+			);
+
+			if($web){
+				$_SESSION['name']=$userData['name'];
+				$_SESSION['id']=$userData['id'];
+				$_SESSION['email']=$userData['email'];
+				$_SESSION['profilePic']=$userData['profilePic'];
+				header("Location: index.php");
+			}
+			else{
+				$tokenQ=mysqli_query($con,"select * from tokens where uid=" . $userData['id']);
+				
+				if(mysqli_num_rows($tokenQ)>=5){
+
+					$time=time();$id=0;
+					for($i=0;$i<mysqli_num_rows($tokenQ);$i++){
+						$row=mysqli_fetch_assoc($tokenQ);
+						if($row['time']<$time){$time=$row['time'];$id=$row['id'];}
+					}
+					mysqli_query($con,"delete from tokens where id=" . $id);
+
+				}
+
+				$randToken=createNewToken($userData['id']);
+				mysqli_query($con,"insert into tokens (uid,token,time) values (" . $userData['id'] . ",'" . $randToken . "','" . time() . "')");
+				echo $randToken . "<>" . $userData['name'] . "<>" . $userData['profilePic'] . "<>" . $userData['email'];
+			}
 		}
 	}
 ?>
